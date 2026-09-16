6b73c10bcf30ad1d1f6902ef3cdd3cef95323cd2
xFactor <xfactorxai@gmail.com>
2026-09-16 01:00:10 -0500

    Prepare privacy-aware Pinterest tag integration

---
+++ components/pinterest-tag.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Script from 'next/script'
+
+const tagId = process.env.NEXT_PUBLIC_PINTEREST_TAG_ID?.trim()
+const enabled = process.env.NEXT_PUBLIC_PINTEREST_TRACKING_ENABLED === 'true'
+
+export function PinterestTag() {
+  if (!tagId || !enabled) return null
+
+  return (
+    <>
+      <Script id="planetx-pinterest-tag" strategy="afterInteractive">
+        {`
+          !function(e){if(!window.pintrk){window.pintrk=function(){window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version="3.0";var t=document.createElement("script");t.async=!0,t.src=e;var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+          pintrk('load', '${tagId}');
+          pintrk('page');
+        `}
+      </Script>
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          alt=""
+          src={`https://ct.pinterest.com/v3/?tid=${tagId}&event=init&noscript=1`}
+        />
+      </noscript>
+    </>
+  )
+}
