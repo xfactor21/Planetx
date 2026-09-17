6b40782c5f33ef6653c7535b6054b3dce20a55e6
xFactor <xfactorxai@gmail.com>
2026-09-16 23:45:57 -0500

    CommandCenter-09.17-v3.2 wire social visibility and interaction boost

--- command-center/src/main.tsx
+++ command-center/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CommandCenterPro from './CommandCenterPro'
+import CommandCenterInteractions from './CommandCenterInteractions'
 import './pro.css'
+import './interactions.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CommandCenterPro />
+    <CommandCenterInteractions />
   </StrictMode>,
 )
